db1b913bfd6ac1d57c8b0cdfcba36f6a944ed5df
emils <emils@gmail.com>
2025-11-10 16:24:50 +0200

    coffee-order.js

---
+++ coffee-order.js
@@ -0,0 +1,9 @@
+let coffeeSize = "large";
+let addSugar = true;
+ if (coffeeSize = "large") {
+console.log("2 eiro")
+ } else if(coffeeSize = "meduim") {
+console.log("1.5 eiro")
+ } else {
+console.log("1 eiro")
+ }
