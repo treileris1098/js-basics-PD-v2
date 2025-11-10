5628454af62b692d1c33ecbeb22acceddf86974d
emils <emils@gmail.com>
2025-11-10 16:46:30 +0200

    books-array.js

---
+++ books-array.js
@@ -0,0 +1,12 @@
+let books=[
+{
+    nosaukums: "vīramāte",autors: "salija",isAvailable: "ir piejama",
+},
+{nosaukums: "nodevejs",autors: "armands",isAvailable: "nav-piejama",}
+];
+
+for(let i=0;i<books.length;i++){
+let gramatas  = books[i]
+    console.log("nosaukums: " ,gramatas.nosaukums, "  ", gramatas.isAvailable)
+
+};
